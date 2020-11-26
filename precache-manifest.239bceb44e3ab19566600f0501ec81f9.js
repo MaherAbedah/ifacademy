47449d0ffa14dd2aaab1af5d58c86070190d1970
Maher Abedah <m.abedah1@gmail.com>
2020-11-26 14:04:44 +0200

    Updates

---
+++ precache-manifest.239bceb44e3ab19566600f0501ec81f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cccbc382885baa37177de260136a4af2",
+    "url": "/ifacademy/index.html"
+  },
+  {
+    "revision": "f963342472b7719b8afe",
+    "url": "/ifacademy/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "53cf3431605c64b8cb07",
+    "url": "/ifacademy/static/js/2.b7e7956c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ifacademy/static/js/2.b7e7956c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f963342472b7719b8afe",
+    "url": "/ifacademy/static/js/main.852a95cb.chunk.js"
+  },
+  {
+    "revision": "710ae60ab4afae06a906",
+    "url": "/ifacademy/static/js/runtime-main.89a3af45.js"
+  }
+]);
