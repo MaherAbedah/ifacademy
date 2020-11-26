7e522f98da634c70f5a984d43ec966183662114a
Maher Abedah <m.abedah1@gmail.com>
2020-11-26 13:47:07 +0200

    adding project files

---
+++ src/components/testimonials.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export class testimonials extends Component {
+  render() {
+    return (
+      <div id="testimonials">
+        <div className="container">
+          <div className="section-title text-center">
+            <h2>Our graet partners</h2>
+          </div>
+          <div className="row">
+            {this.props.data
+              ? this.props.data.map((d, i) => (
+                  <div key={`${d.name}-${i}`} className="col-md-4">
+                    <div className="testimonial">
+                      <div className="testimonial-image">
+                        {" "}
+                       <a href={d.link}> <img src={d.img} alt="" /></a>{" "}
+                      </div>
+                      <div className="testimonial-content">
+                        <h3>{d.title}</h3>
+                        <p>{d.text}</p>
+                        {/* <div className="testimonial-meta"> - {d.name} </div> */}
+                      </div>
+                    </div>
+                  </div>
+                ))
+              : "loading"}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default testimonials;
