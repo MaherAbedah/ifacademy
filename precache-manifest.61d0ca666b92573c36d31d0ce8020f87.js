4bf3dcf6376efaedde183868f9568420f5ebe7bc
Maher Abedah <m.abedah1@gmail.com>
2020-12-15 09:19:13 +0200

    Updates

---
+++ precache-manifest.61d0ca666b92573c36d31d0ce8020f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3e2e24e226d9abe0b9bdbe64504b06",
+    "url": "/ifacademy/index.html"
+  },
+  {
+    "revision": "1af2ba35178a01fde4ea",
+    "url": "/ifacademy/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "38477f80abde31f4a763",
+    "url": "/ifacademy/static/js/2.5331387d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ifacademy/static/js/2.5331387d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1af2ba35178a01fde4ea",
+    "url": "/ifacademy/static/js/main.92619454.chunk.js"
+  },
+  {
+    "revision": "710ae60ab4afae06a906",
+    "url": "/ifacademy/static/js/runtime-main.89a3af45.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ifacademy/precache-manifest.1417e321f37ce764147d1cafce70da76.js"
+  "/ifacademy/precache-manifest.61d0ca666b92573c36d31d0ce8020f87.js"
 );
 
 self.addEventListener('message', (event) => {
